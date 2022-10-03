fadc966d22ced7e3de0fc78b04f73918dc129e52
shazimali <31865659+shazimali@users.noreply.github.com>
2022-10-03 16:35:21 +0500

    remove gates from AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Gate;
-use App\Models\Role;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,23 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(Schema::hasTable('roles')) {
-        $roles = Role::with('permissions')->get();
-        $permissionsArray = [];
-        foreach ($roles as $role) {
-            foreach ($role->permissions as $permissions) {
-                $permissionsArray[$permissions->key][] = $role->id;
-            }
-        }
-        
-        // Every permission may have multiple roles assigned
-        foreach ($permissionsArray as $title => $roles) {
-            Gate::define($title, function ($user) use ($roles) {
-                // We check if we have the needed roles among current user's roles
-                return count(array_intersect($user->roles->pluck('id')->toArray(), $roles)) > 0;
-            });
-        }
-        }
         
     }
 }
